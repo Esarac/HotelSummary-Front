369d8616b210cf9609551d25d15d865cb5a045b8
Johan Sebastián Giraldo Rubio <j.sebasgr036@gmail.com>
2022-11-16 21:56:22 -0500

    Add google map

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Map from './components/Map';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <h1>Map</h1>
+    <Map></Map>
+  </div> 
+);
